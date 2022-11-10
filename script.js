7e5790806fdb21cec79d74ab10ac709ebdd65506
pixxelshaon <saifulislamshaonapk@gmail.com>
2022-11-11 01:07:52 +0600

    okay

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('login-btn').addEventListener('click',function(){
+    document.getElementById('login-page').style.display = 'none';
+    document.getElementById('transaction-area').style.opacity = '1';
+});
